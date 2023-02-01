968092277e578a057db30e7615915cdf60208346
Furkan Aydin <aydinbcs@gmail.com>
2023-02-01 15:05:44 +0300

    scene fix
    -runner path updates

--- Assets/Graup of Runner/Scripts/Obstacle/Obstacle2.cs
+++ Assets/Graup of Runner/Scripts/Obstacle/Obstacle2.cs
@@ -5,10 +5,19 @@ using DG.Tweening;
 
 public class Obstacle2 : MonoBehaviour
 {
+   [SerializeField] private bool wrongWay;
     
     void Awake()
     {
-        transform.DOLocalRotate(new Vector3(0, -90, 0), 0.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InBack);
+        if (!wrongWay)
+        {
+            transform.DOLocalRotate(new Vector3(0, -90, 0), 0.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InBack);
+        }
+        else
+        {
+            transform.DOLocalRotate(new Vector3(0, -270, 0), 0.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InBack);
+        }
+        
     }
 
     
